4f163fe4e6c1ede4531747b2ed896013e98f0cc4
Lucas Alves <lucasalvesnascimento.lan@gmail.com>
2023-10-29 11:18:56 -0300

    Consumindo API e retornando status do Pokémon

--- pokedex/src/app/service/poke-api.service.ts
+++ pokedex/src/app/service/poke-api.service.ts
@@ -2,13 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 //Observable
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeApiService {
-
 
   private url: string = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=100';
   constructor(
@@ -17,10 +16,25 @@ export class PokeApiService {
 
   get apiListAllPokemons():Observable<any>{
     return this.http.get<any>(this.url).pipe(
-      tap(res => res),
-      tap(res => {
-        console.log(res)
+      tap( res => res ),
+      tap( res => {
+        res.results.map( (resPokemons: any) => {
+
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          )
+
+        })
       })
+    )
+  }
+
+  public apiGetPokemons( url: string ):Observable<any>{
+
+    return this.http.get<any>( url ).pipe(
+      map(
+        res => res
+      )
     )
   }
 }
