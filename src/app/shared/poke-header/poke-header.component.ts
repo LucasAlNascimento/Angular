0a5d557810da18344cc02d7685a127cfa1ca58bc
Lucas Alves <lucasalvesnascimento.lan@gmail.com>
2023-11-11 01:42:30 -0300

    Configurando e estilizando light theme
    Configurando variables.scss e criando lógica no componente header para mudar o ícone do light theme para o dark theme e vice-versa.

--- src/app/shared/poke-header/poke-header.component.ts
+++ src/app/shared/poke-header/poke-header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'poke-header',
   templateUrl: './poke-header.component.html',
   styleUrls: ['./poke-header.component.scss']
 })
-export class PokeHeaderComponent {
+export class PokeHeaderComponent implements OnInit {
+  public icon: string = "moon.svg";
+  public logo: string = "logo.svg";
 
-}
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+ 
+  
+  public toggle(){
+    const theme: boolean = document.body.classList.toggle("light-theme");
+
+    if(theme){
+      this.logo = "logo-light.svg";
+      return (this.icon = "sun.svg");
+    }
+      this.logo = "logo.svg";
+      return (this.icon = "moon.svg");
+    }
+  }
+
+
